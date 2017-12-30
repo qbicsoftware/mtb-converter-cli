43bf4f083a31c11a94c5afe48261cde389dbeb08
sven <sven.fillinger@mailbox.org>
2017-12-30 18:25:03 +0100

    Check barcode presence

--- mtbconverter/mtbconverter_exceptions.py
+++ mtbconverter/mtbconverter_exceptions.py
@@ -6,3 +6,6 @@ class MtbUnknownFileType(Exception):
 
 class MtbIncompleteArchive(Exception):
     """Raise, if the MTB archive is missing files"""
+
+class MtbUnknownBarcode(Exception):
+    """Raise, if not all archive members contain the same barcode."""
--- tests/testfiles/test_archive_integrity.py
+++ tests/testfiles/test_archive_integrity.py
@@ -10,6 +10,7 @@ from zipfile import ZipFile
 from mtbconverter.mtbconverter import MtbConverter
 from mtbconverter.mtbconverter_exceptions import MtbUnknownFileType
 from mtbconverter.mtbconverter_exceptions import MtbIncompleteArchive
+from mtbconverter.mtbconverter_exceptions import MtbUnknownBarcode
 
 CURRENT_WD = os.path.dirname(__file__)
 
@@ -21,10 +22,10 @@ class ArchiveTests(unittest.TestCase):
     def test_unknown_file_type(self, MockZipFile):
         ziparchive = MockZipFile()
         ziparchive.infolist.return_value = [
-            'QUK170001AE_germline_snv.tsv',
-            'QUK170001AE_noclue_snv.tsv'
+            'QUK17001AE_germline_snv.tsv',
+            'QUK17001AE_noclue_snv.tsv'
             ]
-        MtbConverter(ziparchive)
+        MtbConverter(ziparchive, "")
 
 
     @patch('zipfile.ZipFile')
@@ -32,24 +33,53 @@ class ArchiveTests(unittest.TestCase):
     def test_incomplete_archive(self, MockZipFile):
         ziparchive = MockZipFile()
         ziparchive.infolist.return_value = [
-            'QUK170001AE_germline_snv.tsv'
+            'QUK17001AE_germline_snv.tsv'
         ]
-        MtbConverter(ziparchive)
+        MtbConverter(ziparchive, "")
+
 
     @patch('zipfile.ZipFile')
     def test_complete_archive(self, MockZipFile):
         ziparchive = MockZipFile()
         ziparchive.infolist.return_value = [
-            'QUK170001AE_germline_snv.tsv',
-            'QUK170001AE_germline_cnv.tsv',
-            'QUK170001AE_somatic_snv.tsv',
-            'QUK170001AE_somatic_cnv.tsv',
-            'QUK170001AE_somatic_sv.tsv',
-            'QUK170001AE_metadata.tsv'
+            'QUK17001AE_germline_snv.tsv',
+            'QUK17001AE_germline_cnv.tsv',
+            'QUK17001AE_somatic_snv.tsv',
+            'QUK17001AE_somatic_cnv.tsv',
+            'QUK17001AE_somatic_sv.tsv',
+            'QUK17001AE_metadata.tsv'
         ]
-        MtbConverter(ziparchive)
-    # Check: The Archive must contain 6 tsv files
+        MtbConverter(ziparchive, "")
 
+
+    @raises(MtbUnknownBarcode)
+    @patch('zipfile.ZipFile')
+    def test_different_barcode(self, MockZipFile):
+        ziparchive = MockZipFile()
+        barcode = "QUK17002AE"
+        ziparchive.infolist.return_value = [
+            'QUK17002AE_germline_snv.tsv',
+            'QUK17002AE_germline_cnv.tsv',
+            'QUK17001AE_somatic_snv.tsv',
+            'QUK17002AE_somatic_cnv.tsv',
+            'QUK17001AE_somatic_sv.tsv',
+            'QUK17002AE_metadata.tsv'
+        ]
+        MtbConverter(ziparchive, barcode)
+
+    @patch('zipfile.ZipFile')
+    def test_common_barcode(self, MockZipFile):
+        ziparchive = MockZipFile()
+        barcode = "QUK17002AE"
+        ziparchive.infolist.return_value = [
+            'QUK17002AE_germline_snv.tsv',
+            'QUK17002AE_germline_cnv.tsv',
+            'QUK17002AE_somatic_snv.tsv',
+            'QUK17002AE_somatic_cnv.tsv',
+            'QUK17002AE_somatic_sv.tsv',
+            'QUK17002AE_metadata.tsv'
+        ]
+        MtbConverter(ziparchive, barcode)
     # Check: The tsv files must follow a naming scheme
     # <barcode>_somatic_snv.tsv
     # <barcode>_somatic_cnv.tsv
