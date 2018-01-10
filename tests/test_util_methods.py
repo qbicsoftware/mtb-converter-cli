9d3db45c690ac9f024f539cbfd5c858dca823495
sven1103 <sven.fillinger@qbic.uni-tuebingen.de>
2018-01-10 16:34:01 +0100

    Add tests for util methods

---
+++ tests/test_util_methods.py
@@ -0,0 +1,27 @@
+"""
+Tests for util methods
+"""
+import os
+import unittest
+from nose.tools import raises
+from mtbconverter import utils
+
+INVALID_BARCODE = "QLAUCH123AE"
+VALID_BARCODE = "QAHJH006A4"
+INVALID_PATH = "my/file/QLAUCH123AE_file.fastq.gz"
+VALID_PATH = "my/file/QAHJH006A4_file.fastq.gz"
+
+class ArchiveTests(unittest.TestCase):
+    """Test suite for util methods check"""
+
+    def test_barcode_integrity(self):
+        self.assertTrue(utils.is_valid_barcode(VALID_BARCODE))
+        self.assertFalse(utils.is_valid_barcode(INVALID_BARCODE))
+
+    @raises(ValueError)
+    def test_invalid_path_for_barcode(self):
+        utils.getbarcode(INVALID_PATH)
+
+    def test_valid_path_for_barcode(self):
+        barcode = utils.getbarcode(VALID_PATH)
+        self.assertEqual(barcode, VALID_BARCODE)
